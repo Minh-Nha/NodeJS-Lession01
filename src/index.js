2b607a885fe185d89f604ff5055170714d058bef
Minh-Nha <nnminhnha1511@gmail.com>
2024-04-15 19:41:19 +0700

    buoi 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello NodeJS");
+import /*subTwoNum, */{ printConsole, sumTwoNum } from "./util.js";
+import {default as subTwoNum} from "./util.js";
+function main() {
+    printConsole("Tong 2 so: " + sumTwoNum(1,2))
+    printConsole("Tong 2 so: " + subTwoNum(1,2))
+}
+
+main();
---
+++ src/util.js
@@ -0,0 +1,11 @@
+export function sumTwoNum(num1, num2) {
+    return num1 + num2;
+}
+
+export function printConsole(msg) {
+    return console.log(msg);
+}
+
+export default function subTwoNum(num1, num2) {
+    return num1 - num2;
+}
